b880c2a5d0ebfde591dbb526c858ca8a1588b8c7
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-11-10 09:59:38 +0100

    feat(users-list): display user

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  constructor(
+    public id: number,
+    public lastname: string,
+    public firstname: string,
+    public age: number,
+  ) {
+  }
+}
+
+const users: User[] = [
+  new User(0, 'Doe', 'John', 23),
+  new User(1, 'Doe', 'Jane', 32),
+];
+
+
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age'];
+  dataSource = users;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
 
 }
