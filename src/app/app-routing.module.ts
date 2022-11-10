2fe7707f8951c206688f643c33e9c7665561e876
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-11-10 10:01:50 +0100

    feat(login): route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from "./users-list/users-list.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path: "",
     component: UsersListComponent
+  },
+  {
+    path: "/login",
+    component: LoginComponent
   }
 ];
 
