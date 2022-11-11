8f55531820b1238197712072eff59d6ef1d32af0
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-11 14:17:51 +0100

    feat: save token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiHelperService } from "../api-helper.service";
+import { TokenStorageService } from "../services/token-storage.service";
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,14 @@ import { ApiHelperService } from "../api-helper.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  login(): void {
+  async login(): Promise<void> {
     const username: string = (document.getElementById('username') as HTMLInputElement).value;
     const password: string = (document.getElementById('password') as HTMLInputElement).value;
-    this.api.post({endpoint: '/auth/login', data: { username, password },}).then(response => console.log(response));
+    const response = await this.api.post({ endpoint: '/auth/login', data: { username, password }, });
+    this.tokenStorageService.save(response.access_token);
   }
 
-  constructor(private api: ApiHelperService) {
+  constructor(private api: ApiHelperService, private tokenStorageService: TokenStorageService,) {
   }
 
   ngOnInit(): void {
