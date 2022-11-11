71e5ce6c55b39493e8d3041c9b1f1ea35e25e910
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-11 15:28:35 +0100

    feat(nav): logout button

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from "../services/token-storage.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -6,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  isLogged = false;
 
-  constructor() {
+  constructor(private tokenStorageService: TokenStorageService, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.isLogged = this.tokenStorageService.isLogged();
   }
+
+  logout(): void {
+    this.tokenStorageService.clear();
+    this.router.navigateByUrl("/login");
+  }
+
 
 }
