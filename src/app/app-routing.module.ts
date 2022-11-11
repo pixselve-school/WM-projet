f9b5550f952e4e2271e810d356c848550815e31d
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-11 15:18:48 +0100

    chore: config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from "./users-list/users-list.component";
 import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   {
     path: "users",
-    component: UsersListComponent
+    component: UsersListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "login",
+    component: LoginComponent
   },
   {
     path: "",
-    component: LoginComponent
+    redirectTo: "login",
+    pathMatch: "full"
   }
 ];
 
