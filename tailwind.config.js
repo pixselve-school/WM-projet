f9d4b18d341f922c46ef45b11530aca60dc179ef
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-19 18:12:21 +0100

    chore: install daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,31 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#efaebf",
+
+          "secondary": "#b52826",
+
+          "accent": "#afea77",
+
+          "neutral": "#282F34",
+
+          "base-100": "#2D414D",
+
+          "info": "#8EC7F6",
+
+          "success": "#13AA4F",
+
+          "warning": "#F4C862",
+
+          "error": "#DD3668",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
