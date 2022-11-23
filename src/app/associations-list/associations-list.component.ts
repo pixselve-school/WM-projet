5539d926cf20b3ac7f7ff252c489844df5aba2d9
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-23 19:18:19 +0100

    feat(associations-list): use data from service

--- src/app/associations-list/associations-list.component.ts
+++ src/app/associations-list/associations-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Association, AssociationsService } from '../associations.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-associations-list',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./associations-list.component.css'],
 })
 export class AssociationsListComponent implements OnInit {
-  constructor() {}
+  associations: Association[] = [];
+  modalOpen = true;
+  loadingCreate = false;
+  formGroup = new FormGroup({
+    name: new FormControl(''),
+  });
 
-  ngOnInit(): void {}
+  constructor(private readonly associationsService: AssociationsService) {}
+
+  ngOnInit(): void {
+    this.getAssociations();
+  }
+
+  getAssociations(): void {
+    this.associationsService.getAssociations().subscribe((associations) => {
+      this.associations = associations;
+    });
+  }
+
+  async addAssociation(event: SubmitEvent): Promise<void> {}
 }
