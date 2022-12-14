89994c5e84c8bdff641460437b7d2e1659d1cc35
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-12-14 18:02:33 +0100

    feat(association-detail): list all members of an association
    close #1

--- src/app/association-detail/association-detail.component.ts
+++ src/app/association-detail/association-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Association, AssociationsService } from '../associations.service';
 
 @Component({
   selector: 'app-association-detail',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./association-detail.component.css'],
 })
 export class AssociationDetailComponent implements OnInit {
-  constructor() {}
+  association!: Association;
+  id: string = this.route.snapshot.paramMap.get('id') ?? '';
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private associationService: AssociationsService,
+    private router: Router
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      const association = await this.associationService
+        .getAssociation(parseInt(this.id))
+        .toPromise();
+      if (association === undefined) throw new Error('Association not found');
+      this.association = association;
+    } catch (e) {
+      this.router.navigate(['/associations']);
+    }
+  }
 }
