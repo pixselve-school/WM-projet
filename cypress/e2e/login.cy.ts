0b6a7125a032f5dcd4bab61d1127ee430a8bb428
Yami <34212910+yami2200@users.noreply.github.com>
2023-01-10 11:07:19 +0100

    fix(test): forget to push little edits from yesterday

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,15 +1,15 @@
 describe("login", () => {
   it("should fail login", () => {
     cy.visit("http://localhost:4200");
-    cy.get("input[name=username]").type("99");
+    cy.get("input[name=username]").type("randomemail@oui.oui");
     cy.get("input[name=password]").type("pass");
     cy.get("button[type=submit]").click();
-    cy.get("div").contains("Incorrect username");
+    cy.get("div").contains("Incorrect email");
   });
   it("should login", () => {
     cy.visit("http://localhost:4200/");
-    cy.get("input[name=username]").type("1");
-    cy.get("input[name=password]").type("password");
+    cy.get("input[name=username]").type("admin@administration.fr");
+    cy.get("input[name=password]").type("admin");
     cy.get("button[type=submit]").click();
     cy.url().should("include", "/users");
   });
